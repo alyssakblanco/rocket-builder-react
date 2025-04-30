b14c65817f1e5813a8a700bd172b3e42f25d58b1
Alyssa Blanco <alyssakblanco@outlook.com>
2025-04-30 16:55:01 -0400

    updated site look

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "/webgl_build/webgl_build.loader.js",
-    dataUrl: "/webgl_build/webgl_build.data",
-    frameworkUrl: "/webgl_build/webgl_build.framework.js",
-    codeUrl: "/webgl_build/webgl_build.wasm",
+    // loaderUrl: "/webgl_build/webgl_build.loader.js",
+    // dataUrl: "/webgl_build/webgl_build.data",
+    // frameworkUrl: "/webgl_build/webgl_build.framework.js",
+    // codeUrl: "/webgl_build/webgl_build.wasm",
+    loaderUrl: "/webgl_build/compressed_build.loader.js",
+    dataUrl: "/webgl_build/compressed_build.data.br",
+    frameworkUrl: "/webgl_build/compressed_build.framework.js.br",
+    codeUrl: "/webgl_build/compressed_build.wasm.br",
   });
 
   const webgl_container = {
@@ -14,9 +18,8 @@ function App() {
     width: "100%"
   };
 
-
   return (
-    <div className="App">
+    <div id="main_content" className="App">
       <div>
         <h1>Rocket Builder: Design, Learn, Launch</h1>
       </div>
